0a4dbf7aaf21e8ea1d3e7864158940c52f01619c
kzetadev <kzeta@naver.com>
2020-03-31 14:56:53 +0900

    deleteMember 만듦 updateMember로 보이는건 기분탓

--- src/main/java/com/example/demo/dao/MemberDAO.java
+++ src/main/java/com/example/demo/dao/MemberDAO.java
@@ -4,4 +4,8 @@ public class MemberDAO {
 	public void insertMember() {
 		System.out.println("추가");
 	}
+	public void updateMember() {
+		System.out.println("멤버 삭제함");
+		System.out.println("ASDASDQWRSKFMLAKSFMLAKF");
+	}
 }
